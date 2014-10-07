3fccaec63d80bac8d35a5187d617d8ea8e8fb685
neoluisx <12siul12@gmail.com>
2014-10-07 23:57:06 +0200

    tercer commit

--- trivia/apps/usuario/views.py
+++ trivia/apps/usuario/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render,render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect 
 from .forms import *
 from .models import *
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm , AuthenticationForm 
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout,authenticate
 
 
 
 # Create your views here.
+def login_usuario(request):
+	if request.method=="POST":
+		form=AuthenticationForm(request.POST)
+		if(form.is_valid()==False):
+			username=request.POST["username"]
+			password=request.POST["password"]
+			resultado=authenticate(username=username,password=password)
+			if resultado:
+				login(request,resultado)
+				request.session["name"]=username
+				return HttpResponseRedirect("/usuario/registro/")
+	form=AuthenticationForm()
+	return render_to_response("blog/principal.html",{"form":form},RequestContext(request))
+
 def Registro_Usuario(request):
 	if request.method=="POST":
 		form = UserCreationForm(request.POST)
@@ -18,6 +32,3 @@ def Registro_Usuario(request):
 			return HttpResponseRedirect("blog/principal")
 	form=UserCreationForm()
 	return render_to_response("usuario/registro.html",{"form":form},RequestContext(request))
-
-def Pagina_Principal(request):
-		return render_to_response("blog/principal.html",{},RequestContext(request))
--- trivia/urls.py
+++ trivia/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include("trivia.apps.usuario.urls")),
+    url(r'^', include("trivia.apps.usuario.urls")),
 )
